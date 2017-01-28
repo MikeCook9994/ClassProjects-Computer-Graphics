e7324c0ef91436983078c2bd66fa98afc9bada46
MikeCook9994 <mcook4728@gmail.com>
2017-01-28 17:16:24 -0600

    fireball is drawing

---
+++ hw2/src/fireball.js
@@ -0,0 +1,89 @@
+function Fireball(context) {
+    this.context = context
+    
+    this.angle = 0;
+    this.colorPalette = {
+        orangeRed: "rgb(216, 40, 0)",
+        orangeYellow: "rgb(252, 152, 56)",
+        white: "rgb(252, 252, 252)"
+    }
+}
+
+Fireball.prototype.Draw = function(dtheta, scale){
+    this.angle += dtheta;
+    this.context.save();
+    this.context.translate(256, 256);
+    this.context.rotate(this.angle * Math.PI/180);
+    this.context.scale(scale, scale);
+    DrawFireball(this.context, this.colorPalette);
+    this.context.restore();
+}
+
+function DrawFireball(context, colorPalette) {
+    context.fillStyle = colorPalette.orangeRed;
+    DrawLargestFireballPiece(context);
+
+    context.fillStyle = colorPalette.orangeYellow;
+    DrawMediumFireballPiece(context);
+
+    context.fillStyle = colorPalette.white;
+    DrawFireballAccents(context);
+}
+
+function DrawLargestFireballPiece(context) {
+    context.beginPath();
+    context.moveTo(0, 6);
+    context.lineTo(0, 2);
+    context.lineTo(1, 2);
+    context.lineTo(1, 1);
+    context.lineTo(2, 1);
+    context.lineTo(2, 0);
+    context.lineTo(6, 0);
+    context.lineTo(6, 1);
+    context.lineTo(7, 1);
+    context.lineTo(7, 4);
+    context.lineTo(6, 4);
+    context.lineTo(6, 5);
+    context.lineTo(4, 5);
+    context.lineTo(4, 7);
+    context.lineTo(5, 7);
+    context.lineTo(5, 8);
+    context.lineTo(3, 8);
+    context.lineTo(3, 7);
+    context.lineTo(1, 7);
+    context.lineTo(1, 6);
+    context.lineTo(0, 6);
+    context.fill();
+    context.closePath();
+
+    context.fillRect(5, 6, 1, 1);
+    context.fillRect(6, 7, 1, 1);
+    context.fillRect(7, 5, 1, 1);
+}
+
+function DrawMediumFireballPiece(context) {
+    context.beginPath();
+    context.moveTo(1, 5);
+    context.lineTo(1, 3);
+    context.lineTo(2, 3);
+    context.lineTo(2, 2);
+    context.lineTo(3, 2);
+    context.lineTo(3, 1);
+    context.lineTo(5, 1);
+    context.lineTo(5, 2);
+    context.lineTo(6, 2);
+    context.lineTo(6, 3);
+    context.lineTo(5, 3);
+    context.lineTo(5, 4);
+    context.lineTo(3, 4);
+    context.lineTo(3, 6);
+    context.lineTo(2, 6);
+    context.lineTo(2, 5);
+    context.closePath();
+    context.fill();
+}
+
+function DrawFireballAccents(context) {
+    context.fillRect(2, 3, 1, 1);
+    context.fillRect(3, 2, 1, 1);
+}
--- hw2/src/index.js
+++ hw2/src/index.js
@@ -1,13 +1,29 @@
 (() => {
     let canvas = document.getElementById("canvas");
     let context = canvas.getContext('2d');
-    let scene = new Scene(context, canvas.height, canvas.width);
     let blockSize = document.getElementById("blockSize");
+    let fireballAngle = document.getElementById("fireballAngle");
+
+    let scene = new Scene(context, canvas.height, canvas.width);
+    let fireball = new Fireball(context);
 
     scene.Draw(4);
+    fireball.Draw(0, 16);
+
+    fireballAngle.addEventListener('input', () => {
+        scene.Draw(blockSize.value);
+        fireball.Draw(fireballAngle.value, 16);
+    });
 
     blockSize.addEventListener('input', () => {
         scene.Draw(blockSize.value);
+        fireball.Draw(fireballAngle.value, 16);
     });
+    
+    // let i = 0;
+    // setInterval(() => {
+    //     fireball.Draw(i, 16);
+    //     i++;
+    // }, 100);
 })();
 

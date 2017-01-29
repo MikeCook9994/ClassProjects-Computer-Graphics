58f81133bf14466aed1f0080a78c3a5d4198fd60
MikeCook9994 <mcook4728@gmail.com>
2017-01-29 00:18:02 -0600

    fire bar is working!

--- hw2/src/firebar.js
+++ hw2/src/firebar.js
@@ -3,6 +3,7 @@ function Firebar(context) {
         orangeBrown: "rgb(116, 116, 116)",
         gray: "rgb(200, 76, 12)"
     }
+    this.angle = 0;
 
     this.context = context;
 
@@ -15,12 +16,9 @@ function Firebar(context) {
 Firebar.prototype.Draw = function(x, fireRotationDelta, fireScale) {
     this.context.save();
     this.context.translate(x, 320);
-
-    this.context.save();
     this.context.scale(4, 4);
     DrawBlock(this.context, this.colorPalette);
-    this.context.restore();
-
+    this.angle = DrawFireballBar(this.context,this.fireballs, fireScale, this.angle);
     this.context.restore();
 }
 
@@ -37,4 +35,25 @@ function DrawBlock(context, colorPalette) {
     context.fillRect(13, 2, 1, 1);
     context.fillRect(2, 13, 1, 1);
     context.fillRect(13, 13, 1, 1);
+}
+
+function DrawFireballBar(context, fireballs, fireScale, currAngle) {
+    context.save();
+    context.translate(8, 8);
+
+    let angle = currAngle + (2 * (Math.PI / 180));
+    context.rotate(angle);
+    fireballs[0].Draw(5, fireScale);
+
+    for(let i = 1; i < 7; i++) {
+        context.save();
+        context.translate(0, -fireScale * 8);
+        fireballs[0].Draw(1, fireScale);
+    }
+
+    for(let i = 0; i < 7; i++) {
+        context.restore();
+    }
+    
+    return angle;
 }

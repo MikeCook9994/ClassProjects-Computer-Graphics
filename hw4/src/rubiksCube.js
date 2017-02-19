9e74879f743ba1446944091d8ff2003610ada1a9
MikeCook9994 <mcook4728@gmail.com>
2017-02-19 11:11:48 -0600

    it looks like a rubiks cube

--- hw4/src/rubiksCube.js
+++ hw4/src/rubiksCube.js
@@ -26,16 +26,7 @@ RubiksCube.prototype.Draw = function(transformation, blowOutSize, drawWireFrameO
 							(rowIndex == 0 ? blowOutSize: ((rowIndex == 1 ? 0 : -blowOutSize) + (-10 * rowIndex)))
 						]), centeringTransform);
 
-				let zero = DegreesToRadians(0);
-				let ninety = DegreesToRadians(90);
-				let oneeighty = DegreesToRadians(180);
-				let twoseventy = DegreesToRadians(270);
-
-				let rotations = { 
-					x : zero,
-					y : zero, 
-					z : zero 
-				};
+				let rotations = handleOrientation(slice, rowIndex, blockIndex);
 
 				let blockRotationTransform = m4.multiply(m4.rotationX(rotations.x), m4.multiply(m4.rotationY(rotations.y), m4.multiply(m4.rotationZ(rotations.z), blockTranslationTransform)));
 				block.Draw(blockRotationTransform);
@@ -50,7 +41,7 @@ RubiksCube.prototype.Draw = function(transformation, blowOutSize, drawWireFrameO
 function InitializePieces(cubePieces, threeDimContext) {
 	cubePieces.top[0][0] = new CornerPiece(threeDimContext, "green", "white", "orange");
 	cubePieces.top[0][1] = new EdgePiece(threeDimContext, "green", "white");
-	cubePieces.top[0][2] = new CornerPiece(threeDimContext, "red", "white", "green");
+	cubePieces.top[0][2] = new CornerPiece(threeDimContext, "green", "red", "white");
 
 	cubePieces.top[1][0] = new EdgePiece(threeDimContext, "orange", "white");
 	cubePieces.top[1][1] = new CenterPiece(threeDimContext, "white");
@@ -58,7 +49,7 @@ function InitializePieces(cubePieces, threeDimContext) {
 	
 	cubePieces.top[2][0] = new CornerPiece(threeDimContext, "white", "blue", "orange");
 	cubePieces.top[2][1] = new EdgePiece(threeDimContext, "white", "blue");
-	cubePieces.top[2][2] = new CornerPiece(threeDimContext, "red", "blue", "white");
+	cubePieces.top[2][2] = new CornerPiece(threeDimContext, "white", "red", "blue");
 
 	cubePieces.middle[0][0] = new EdgePiece(threeDimContext, "green", "orange");
 	cubePieces.middle[0][1] = new CenterPiece(threeDimContext, "green");
@@ -74,15 +65,105 @@ function InitializePieces(cubePieces, threeDimContext) {
 
 	cubePieces.bottom[0][0] = new CornerPiece(threeDimContext, "yellow", "green", "orange");
 	cubePieces.bottom[0][1] = new EdgePiece(threeDimContext, "yellow", "green");
-	cubePieces.bottom[0][2] = new CornerPiece(threeDimContext, "yellow", "green", "red");
+	cubePieces.bottom[0][2] = new CornerPiece(threeDimContext, "yellow", "red", "green");
 
-	cubePieces.bottom[1][0] = new EdgePiece(threeDimContext, "yellow", "orange");
+	cubePieces.bottom[1][0] = new EdgePiece(threeDimContext, "orange", "yellow");
 	cubePieces.bottom[1][1] = new CenterPiece(threeDimContext, "yellow");
-	cubePieces.bottom[1][2] = new EdgePiece(threeDimContext, "yellow", "red");
+	cubePieces.bottom[1][2] = new EdgePiece(threeDimContext, "red", "yellow");
 
-	cubePieces.bottom[2][0] = new CornerPiece(threeDimContext, "yellow", "blue", "orange");
-	cubePieces.bottom[2][1] = new EdgePiece(threeDimContext, "yellow", "blue");
-	cubePieces.bottom[2][2] = new CornerPiece(threeDimContext, "yellow", "blue", "red");
+	cubePieces.bottom[2][0] = new CornerPiece(threeDimContext, "blue", "yellow", "orange");
+	cubePieces.bottom[2][1] = new EdgePiece(threeDimContext, "blue", "yellow");
+	cubePieces.bottom[2][2] = new CornerPiece(threeDimContext, "blue", "red", "yellow");
+}
+
+function handleOrientation(slice, rowIndex, blockIndex) {
+	let zero = DegreesToRadians(0);
+	let ninety = DegreesToRadians(90);
+	let oneeighty = DegreesToRadians(180);
+	let twoseventy = DegreesToRadians(270);
+
+	let rotations = { 
+		x : zero,
+		y : zero, 
+		z : zero 
+	};
+
+	if(slice == "top") {
+		if(rowIndex == 0) {
+			if(blockIndex == 2) {
+				rotations.z = twoseventy;
+			}
+		}
+		else if(rowIndex == 1) {
+			if(blockIndex == 0) {
+				rotations.y = twoseventy;
+			}
+			else if(blockIndex == 2){
+				rotations.y = ninety;
+			}
+		}
+		else if(rowIndex == 2){
+			rotations.x = twoseventy;
+			if(blockIndex == 2) {
+				rotations.y = twoseventy;
+			}
+		}
+	}
+	else if(slice == "middle") {
+		if(rowIndex == 0) {
+			if(blockIndex == 0) {
+				rotations.z = ninety;
+			}
+			else if(blockIndex == 1) {
+				rotations.x = ninety;
+			}
+			else if(blockIndex == 2) {
+				rotations.z = twoseventy;
+			}
+		}
+		else if(rowIndex == 1) {
+			if(blockIndex == 0) {
+				rotations.z = ninety;
+			}
+			else if(blockIndex == 2){
+				rotations.z = twoseventy;
+			}
+		}
+		else if(rowIndex == 2){
+			rotations.x = twoseventy;
+			if(blockIndex == 0) {
+				rotations.z = ninety;
+			}
+			else if(blockIndex == 2) {
+				rotations.z = twoseventy;
+			}
+		}
+	}
+	else if(slice == "bottom") {
+		if(rowIndex == 0) {
+			rotations.x = ninety;
+			if(blockIndex == 2) {
+				rotations.y = ninety;
+			}
+		}
+		else if(rowIndex == 1) {
+			rotations.x = oneeighty;
+			if(blockIndex == 0) {
+				rotations.y = ninety;
+			}
+			else if(blockIndex ==2){
+				rotations.y = twoseventy;
+			}
+		}
+		else if(rowIndex ==2) {
+			rotations.x = oneeighty;
+			if(blockIndex == 2){
+				rotations.z = ninety;
+			}
+		}
+	}
+
+	return rotations
 }
 
 function DegreesToRadians(degrees) {

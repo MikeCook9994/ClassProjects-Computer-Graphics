72f96bb53cce11abf90bb211ad9f9b7bc9cd8ba5
MikeCook9994 <mcook4728@gmail.com>
2017-03-08 23:55:44 -0600

    have the example code working

--- webGLTest/index.js
+++ webGLTest/index.js
@@ -4,15 +4,17 @@ let m4 = twgl.m4;
     let canvas = document.getElementById("drawing-plane");
     let gl = canvas.getContext("webgl");
 
-    let vertexShaderSource = document.getElementById("vertex-shader").text;
-    let fragmentShaderSource = document.getElementById("fragment-shader").text;
+    let slider1 = document.getElementById('slider1');
+    slider1.value = 0;
+    let slider2 = document.getElementById('slider2');
+    slider2.value = 0;
 
     let vertexShader = gl.createShader(gl.VERTEX_SHADER);
-    gl.shaderSource(vertexShader, vertexShaderSource);
+    gl.shaderSource(vertexShader, triangleVertexShader);
     gl.compileShader(vertexShader);
     
     let fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
-    gl.shaderSource(fragmentShader, fragmentShaderSource);
+    gl.shaderSource(fragmentShader, triangleFragmentShader);
     gl.compileShader(fragmentShader);
 
     let shaderProgram = gl.createProgram();
@@ -21,10 +23,10 @@ let m4 = twgl.m4;
     gl.linkProgram(shaderProgram);
     gl.useProgram(shaderProgram);
 
-    let positionAttribute = gl.getAttributeLocation(shaderProgram, "vPosition");
+    let positionAttribute = gl.getAttribLocation(shaderProgram, "vPosition");
     gl.enableVertexAttribArray(positionAttribute);
 
-    let colorAttribute = gl.getAttributeLocation(shaderProgram, "vColor");
+    let colorAttribute = gl.getAttribLocation(shaderProgram, "vColor");
     gl.enableVertexAttribArray(colorAttribute);
 
     let mvpMatrix = gl.getUniformLocation(shaderProgram, "uMVP");
@@ -52,43 +54,42 @@ let m4 = twgl.m4;
     let colorBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, vertexColors, gl.STATIC_DRAW);
-    trianglePosBuffer.itemSize = 3;
-    trianglePosBuffer.numItems = 12;
+    colorBuffer.itemSize = 3;
+    colorBuffer.numItems = 12;
 
+    function draw() {
+        let angle1 = slider1.value*0.01*Math.PI;
+        let angle2 = slider2.value*0.01*Math.PI;
+
+        // Circle around the y-axis
+        var eye = [300*Math.sin(angle1),150.0,300.0*Math.cos(angle1)];
+        var target = [0,40,0];
+        var up = [0,1,0];
+
+        var tModel = m4.multiply(m4.scaling([100,100,100]),m4.axisRotation([1,1,1],angle2));
+        var tCamera = m4.inverse(m4.lookAt(eye,target,up));
+        var tProjection = m4.perspective(Math.PI/4,1,10,1000);
+
+        var tMVP=m4.multiply(m4.multiply(tModel,tCamera),tProjection);
+
+        // Clear screen, prepare for rendering
+        gl.clearColor(0.0, 0.0, 0.0, 1.0);
+        gl.enable(gl.DEPTH_TEST);
+        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+        // Set up uniforms & attributes
+        gl.uniformMatrix4fv(mvpMatrix, false, tMVP);
+                    
+        gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+        gl.vertexAttribPointer(colorAttribute, colorBuffer.itemSize, gl.FLOAT, false, 0, 0);
+        gl.bindBuffer(gl.ARRAY_BUFFER, trianglePosBuffer);
+        gl.vertexAttribPointer(positionAttribute, trianglePosBuffer.itemSize, gl.FLOAT, false, 0, 0);
+
+        // Do the drawing
+        gl.drawArrays(gl.TRIANGLES, 0, trianglePosBuffer.numItems);
+    }
+
+    slider1.addEventListener("input",draw);
+    slider2.addEventListener("input",draw);
     draw();
 })();
-
-function draw() {
-    // Circle around the y-axis
-    var eye = [300*Math.sin(3.14159),150.0,300.0*Math.cos(3.14159/2)];
-    var target = [0,40,0];
-    var up = [0,1,0];
-
-    var tModel = m4.multiply(m4.scaling([100,100,100]),m4.axisRotation([1,1,1],angle2));
-    var tCamera = m4.inverse(m4.lookAt(eye,target,up));
-    var tProjection = m4.perspective(Math.PI/4,1,10,1000);
-
-    var tMVP=m4.multiply(m4.multiply(tModel,tCamera),tProjection);
-
-    // Clear screen, prepare for rendering
-    gl.clearColor(0.0, 0.0, 0.0, 1.0);
-    gl.enable(gl.DEPTH_TEST);
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-
-    // Set up uniforms & attributes
-    gl.uniformMatrix4fv(shaderProgram.MVPmatrix,false,tMVP);
-                
-    gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
-    gl.vertexAttribPointer(shaderProgram.ColorAttribute, colorBuffer.itemSize,
-        gl.FLOAT,false, 0, 0);
-    gl.bindBuffer(gl.ARRAY_BUFFER, trianglePosBuffer);
-    gl.vertexAttribPointer(shaderProgram.PositionAttribute, trianglePosBuffer.itemSize,
-        gl.FLOAT, false, 0, 0);
-
-    // Do the drawing
-    gl.drawArrays(gl.TRIANGLES, 0, trianglePosBuffer.numItems);
-}
-
-function readShaderFile(shaderPath) {
-
-}

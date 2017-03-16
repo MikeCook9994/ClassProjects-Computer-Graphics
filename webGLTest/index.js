7e30c3b64a684f61a92990e2ca10d839c668cfba
MikeCook9994 <mcook4728@gmail.com>
2017-03-16 10:24:23 -0500

    currently doesn't work; refactoring code

--- webGLTest/index.js
+++ webGLTest/index.js
@@ -2,7 +2,7 @@ let m4 = twgl.m4;
 
 (() => {
     let canvas = document.getElementById("drawing-plane");
-    let gl = canvas.getContext("webgl");
+    let glHost = new GLHost(canvas.getContext("webgl"));
 
     let slider1 = document.getElementById("slider1");
     let slider2 = document.getElementById("slider2");
@@ -10,61 +10,35 @@ let m4 = twgl.m4;
 
     // change what is assigned to these variables to change the model and the shader;
     let objectAttributes = pipeObjectAttributes;
-    let vertexShaderSource = shadingVertexShader;
-    let fragmentShaderSource = shadingFragmentShader;
 
-    // compile vertex shader
-    let vertexShader = gl.createShader(gl.VERTEX_SHADER);
-    gl.shaderSource(vertexShader, vertexShaderSource);
-    gl.compileShader(vertexShader);
+    let vertexShader = glHost.CreateAndCompileShader(glHost.gl.VERTEX_SHADER, shadingVertexShader);
+    if(vertexShader == null) {
+        return null;
+    } 
     
-    // compile fragment shader
-    let fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
-    gl.shaderSource(fragmentShader, fragmentShaderSource);
-    gl.compileShader(fragmentShader);
+    let fragmentShader = glHost.CreateAndCompileShader(glHost.gl.FRAGMENT_SHADER, shadingFragmentShader);
+    if(fragmentShader == null) {
+        return null;
+    } 
 
-    // create shader program and attach shaders
-    let shaderProgram = gl.createProgram();
-    gl.attachShader(shaderProgram, vertexShader);
-    gl.attachShader(shaderProgram, fragmentShader);
-    gl.linkProgram(shaderProgram);
-    gl.useProgram(shaderProgram);
+    let shaderProgram = glHost.CreateAndConfigureProgram(vertexShader, fragmentShader);
+    if(shaderProgram == null) {
+        return null;
+    } 
 
-    // set up our uniforms
-    let modelViewMatrix = gl.getUniformLocation(shaderProgram, "modelViewMatrix");
-    let normalMatrix = gl.getUniformLocation(shaderProgram, "normalMatrix");
-    let projectionMatrix = gl.getUniformLocation(shaderProgram, "projectionMatrix");
-    let time = gl.getUniformLocation(shaderProgram, "time");
-    let wireframe = gl.getUniformLocation(shaderProgram, "wireframe");
+    let uniformNames = ["modelViewMatrix", "normalMatrix", "projectionMatrix", "time", "wireframe"];
+    let uniformLocations = glHost.GetUniformLocations(shaderProgram, uniformNames);
 
     // set up our attributes
-    let positionAttribute = gl.getAttribLocation(shaderProgram, "position");
-    gl.enableVertexAttribArray(positionAttribute);
+    let attributeNames = ["position", "normal", "barycentric"];
+    let attributeLocations = glHost.GetAttributeLocations(shaderProgram, attributeNames);
 
-    let normalAttribute = gl.getAttribLocation(shaderProgram, "normal");
-    gl.enableVertexAttribArray(normalAttribute);
+    let posBuffer = glHost.BindAndBufferData(glHost.gl.ARRAY_BUFFER, new Float32Array(objectAttributes.vertices));
 
-    let barycentricAttribute = gl.getAttribLocation(shaderProgram, "barycentric");
-    gl.enableVertexAttribArray(barycentricAttribute);
+    let normalBuffer = glHost.BindAndBufferData(glHost.gl.ARRAY_BUFFER, new Float32Array(objectAttributes.vertexNormals));
 
-    let posBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, posBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(objectAttributes.vertices), gl.STATIC_DRAW);
-
-    let normalBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, normalBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(objectAttributes.vertexNormals), gl.STATIC_DRAW);
-
-    let barycentricCoords = [];
-    for(let i = 0; i < objectAttributes.vertices.length / 3; i++) {
-        barycentricCoords.push(((i % 3 == 0) ? (1.0) : (0.0)));
-        barycentricCoords.push(((i % 3 == 1) ? (1.0) : (0.0)));
-        barycentricCoords.push(((i % 3 == 2) ? (1.0) : (0.0)));
-    }
-
-    let barycentricBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, barycentricBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(barycentricCoords), gl.STATIC_DRAW);
+    let barycentricCoords = defineBaryCentricCoordiantes(objectAttributes.vertices.length / 3);
+    let barycentricBuffer = glHost.BindAndBufferData(glHost.gl.ARRAY_BUFFER, new Float32Array(barycentricCoords));
 
     function draw() {
         let angle1 = slider1.value*0.01*Math.PI;
@@ -83,33 +57,33 @@ let m4 = twgl.m4;
         let projectionTransform = m4.perspective(Math.PI/4,1,10,1000);
 
         // Clear screen, prepare for rendering
-        gl.clearColor(0.0, 0.0, 0.0, 1.0);
-        gl.enable(gl.DEPTH_TEST);
-        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+        glHost.gl.clearColor(0.0, 0.0, 0.0, 1.0);
+        glHost.gl.enable(glHost.gl.DEPTH_TEST);
+        glHost.gl.clear(glHost.gl.COLOR_BUFFER_BIT | glHost.gl.DEPTH_BUFFER_BIT);
 
         // Set up uniforms & attributes
-        gl.uniformMatrix4fv(modelViewMatrix, false, modelViewTransform);
-        gl.uniformMatrix4fv(normalMatrix, false, normalTransform);
-        gl.uniformMatrix4fv(projectionMatrix, false, projectionTransform);
-        gl.uniform1f(time, + new Date());
+        glHost.gl.uniformMatrix4fv(modelViewMatrix, false, modelViewTransform);
+        glHost.gl.uniformMatrix4fv(normalMatrix, false, normalTransform);
+        glHost.gl.uniformMatrix4fv(projectionMatrix, false, projectionTransform);
+        glHost.gl.uniform1f(time, + new Date());
         if(checkbox1.checked) {
-            gl.uniform1f(wireframe, 1.0);
+            glHost.gl.uniform1f(wireframe, 1.0);
         }
         else {
-            gl.uniform1f(wireframe, false, 0.0);
+            glHost.gl.uniform1f(wireframe, false, 0.0);
         }
         
-        gl.bindBuffer(gl.ARRAY_BUFFER, posBuffer);
-        gl.vertexAttribPointer(positionAttribute, 3, gl.FLOAT, false, 0, 0);
+        // glHost.gl.bindBuffer(glHost.gl.ARRAY_BUFFER, posBuffer);
+        // glHost.gl.vertexAttribPointer(positionAttribute, 3, glHost.gl.FLOAT, false, 0, 0);
         
-        gl.bindBuffer(gl.ARRAY_BUFFER, normalBuffer);
-        gl.vertexAttribPointer(normalAttribute, 3, gl.FLOAT, false, 0, 0);
+        // glHost.gl.bindBuffer(glHost.gl.ARRAY_BUFFER, normalBuffer);
+        // glHost.gl.vertexAttribPointer(normalAttribute, 3, glHost.gl.FLOAT, false, 0, 0);
 
-        gl.bindBuffer(gl.ARRAY_BUFFER, barycentricBuffer);
-        gl.vertexAttribPointer(barycentricAttribute, 3, gl.FLOAT, false, 0, 0);
+        // glHost.gl.bindBuffer(glHost.gl.ARRAY_BUFFER, barycentricBuffer);
+        // glHost.gl.vertexAttribPointer(barycentricAttribute, 3, glHost.gl.FLOAT, false, 0, 0);
 
         // Do the drawing
-        gl.drawArrays(gl.TRIANGLES, 0, objectAttributes.vertices.length / 3);
+        glHost.gl.drawArrays(glHost.gl.TRIANGLES, 0, objectAttributes.vertices.length / 3);
     }
 
     slider1.addEventListener("input",draw);
@@ -117,3 +91,13 @@ let m4 = twgl.m4;
     checkbox1.addEventListener("change", draw);
     draw();
 })();
+
+function defineBaryCentricCoordiantes(length) {
+    let barycentricCoords = [];
+    for(let i = 0; i < length / 3; i++) {
+        barycentricCoords.push(((i % 3 == 0) ? (1.0) : (0.0)));
+        barycentricCoords.push(((i % 3 == 1) ? (1.0) : (0.0)));
+        barycentricCoords.push(((i % 3 == 2) ? (1.0) : (0.0)));
+    }
+    return barycentricCoords;
+} 

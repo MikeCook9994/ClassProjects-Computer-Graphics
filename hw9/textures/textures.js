f7d683dfca2d970fca866649c0180981a361ac89
MikeCook9994 <mjcook3@wisc.edu>
2017-04-20 12:22:34 -0500

    using a high res skybox texture now

--- hw9/textures/textures.js
+++ hw9/textures/textures.js
@@ -4,4 +4,4 @@ let undergroundBlockTextureImageSource = "http://i.imgur.com/w34IpSn.png";
 let brickTextureImageSource = "http://i.imgur.com/VuAeSyH.png";
 let undergroundBrickTextureImageSource = "http://i.imgur.com/5meN48n.png";
 let coinTextureImageSource = "http://i.imgur.com/xWPJ1tJ.png";
-let skyboxImageSource = "http://i.imgur.com/18XEiPu.png";
+let skyboxImageSource = "http://i.imgur.com/dP6Gb26.png";
